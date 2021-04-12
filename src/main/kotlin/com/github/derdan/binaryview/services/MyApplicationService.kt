f48d35570bf069b5f9115e952af1c1ae1403854b
GitHub Action <action@github.com>
2021-04-12 17:09:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/derdan/binaryview/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/derdan/binaryview/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.derdan.binaryview.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.derdan.binaryview.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/derdan/binaryview/services/MyProjectService.kt
+++ src/main/kotlin/com/github/derdan/binaryview/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.derdan.binaryview.services
 
+import com.github.derdan.binaryview.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
